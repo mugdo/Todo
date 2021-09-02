40d5db3a501fae30b559bb52392e2921a18ab990
Shaiful Islam <shaiful@Shaifuls-MacBook-Air.local>
2021-09-02 12:51:16 +0600

    auit

--- todo/todo.go
+++ todo/todo.go
@@ -1,13 +1,1 @@
-package todo
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/globalsign/mgo"
-)
-
-func Init(r *gin.RouterGroup, dbSession *mgo.Session) {
-	repoService := NewRepository(dbSession)
-	authService := NewAuthService(repoService)
-	MakeHTTPHandlers(r, authService)
-
-}
